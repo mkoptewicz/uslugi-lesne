58a19c07b76d222f959ce59984331876fbf2b065
Michal Koptewicz <mkoptewicz@gmail.com>
2020-11-10 21:47:59 +0100

    change mobile menu

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const btn = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
-const showNav = () => nav.classList.toggle("nav--visible");
+const showNav = () => {
+  nav.classList.toggle("nav--visible");
+  btn.classList.toggle("active");
+};
 btn.addEventListener("click", showNav);
