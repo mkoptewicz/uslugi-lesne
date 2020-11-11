285219f9a069f79435262c4bca813ff5db0c0694
Michal Koptewicz <mkoptewicz@gmail.com>
2020-11-11 13:32:28 +0100

    change hero_text content

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 const slideNav = () => {
+ 
   nav.classList.toggle("nav--visible");
   btn.classList.toggle("active");
 };
